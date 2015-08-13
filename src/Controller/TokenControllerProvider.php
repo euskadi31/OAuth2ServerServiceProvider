c60744976081cd35d0a5e21d75be3e1451f1a183
Axel Etcheverry <euskadi31@gmail.com>
2015-08-13 19:15:46 +0200

    chore(Component/Security): WIP

---
+++ src/Controller/TokenControllerProvider.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of the OAuth2ServerServiceProvider.
+ *
+ * (c) Axel Etcheverry <axel@etcheverry.biz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Euskadi31\Silex\Controller;
+
+use Silex\Application;
+use Silex\Api\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * TokenControllerProvider
+ *
+ * @author Axel Etcheverry <axel@etcheverry.biz>
+ */
+class TokenControllerProvider implements ControllerProviderInterface
+{
+    /**
+     * @param  Silex\Application $app
+     * @return Silex\ControllerCollection
+     */
+    public function connect(Application $app)
+    {
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get('/oauth/token', function(Request $request) use ($app) {
+            return '';
+        });
+
+        return $controllers;
+    }
+}
