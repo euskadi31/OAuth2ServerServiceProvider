deb5734337c9ef7810840f4e54d495ba2fd66f6e
Axel Etcheverry <euskadi31@gmail.com>
2015-08-14 16:46:24 +0200

    chore(Component/Security): WIP

--- src/Component/Security/GrantType/PasswordGrantType.php
+++ src/Component/Security/GrantType/PasswordGrantType.php
@@ -12,11 +12,13 @@ namespace Euskadi31\Component\Security\GrantType;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Euskadi31\Component\Security\Core\Exception\OAuthInvalidRequestException;
 
 /**
  * PasswordGrantType
  *
  * @author Axel Etcheverry <axel@etcheverry.biz>
+ * @see https://tools.ietf.org/html/rfc6749#section-4.3
  */
 class PasswordGrantType implements GrantTypeInterface
 {
@@ -33,6 +35,19 @@ class PasswordGrantType implements GrantTypeInterface
      */
     public function handle(Request $request)
     {
+        $username   = $request->request->get('username');
+        $password   = $request->request->get('password');
+        $scope      = $request->request->get('scope');
+
+        if (empty($username)) {
+            throw new OAuthInvalidRequestException('Missing username parameter.');
+        }
+
+        if (empty($password)) {
+            throw new OAuthInvalidRequestException('Missing password parameter.');
+        }
+
+
         return new Response('grant_type=password');
     }
 }
